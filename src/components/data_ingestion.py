26310c5b6d4a18c6b0871dfce3d0561c3bdfc429
xajeel <sajeel135@gmail.com>
2024-06-27 22:11:29 -0700

    Updates

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,59 +6,61 @@ from src.exception import CustomException
 from dataclasses import dataclass
 from config import DataIngestionConfig
 
-
 from data_cleaning import DataCleaning
 from data_transformation import DataTransformation
 from model_trainer import ModelTrainer
-# Purpose of Data Ingestion File is to read the Data from Data Source
-# Split the Data in Train and Test datasets
-
-# Input to our Data ingestion class will be given through this class 
-
 
 class DataIngestion:
 
     def __init__(self):
-        self.ingestion_config= DataIngestionConfig()
+        self.ingestion_config = DataIngestionConfig()
 
     def data_ingestion(self):
         logging.info("Data Ingestion Method Starts")
-
         try:
-        #We can read the data from any data source here at this line            
+            # Read the data from any data source here at this line            
             df = pd.read_csv(r'notebook\data\laptop_data.csv')
             logging.info("Dataset read as pandas DataFrame")
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
 
-        # saving the Raw Data 
+            # Saving the Raw Data 
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
             logging.info('Ingestion of Data Completed')
 
-            return (
-                self.ingestion_config.raw_data_path
-            )
+            return self.ingestion_config.raw_data_path
 
         except Exception as e:
             raise CustomException(e, sys)
-        
+
 
 if __name__ == '__main__':
-    ingestion_object = DataIngestion()
-    ingestion_object.data_ingestion()
+    try:
+        # Data Ingestion
+        ingestion_object = DataIngestion()
+        raw_data_path = ingestion_object.data_ingestion()
 
-# Data Cleaning 
-    final_obj = DataCleaning()
-    final_obj.data_cleaning(final_obj.data_cleaning_config.raw_data)
+        # Data Cleaning
+        final_obj = DataCleaning()
+        final_obj.data_cleaning(raw_data_path)
+        logging.info('Data Cleaning Completed')
 
-# Data Transformation File 
-    transform_object = DataTransformation()
-    transform_object.Data_spliting()
-    transform_object.processed_data()
+        # Data Transformation
+        transform_object = DataTransformation()
+        transform_object.Data_spliting()
+        transform_object.processed_data()
+        logging.info('Data Transformation Completed')
 
-# Model Trainer 
+        # Model Training
+        model_object = ModelTrainer()
+        input_train, input_test = transform_object.processed_data()
+        model_object.model_trainer(
+            x_train=input_train[:, :-1], 
+            y_train=input_train[:, -1], 
+            x_test=input_test[:, :-1], 
+            y_test=input_test[:, -1]
+        )
+        logging.info('Model Training Completed')
 
-    model_object = ModelTrainer()
-    data_transformation_object = DataTransformation()
-    input_train, input_test = data_transformation_object.processed_data()
-    model_object.model_trainer(x_train=input_train[:,:-1], y_train=input_train[:,-1], x_test=input_test[:,:-1], y_test=input_test[:,-1])
-
+    except Exception as e:
+        logging.error("Error in main execution: %s", str(e))
+        raise CustomException(e, sys)
